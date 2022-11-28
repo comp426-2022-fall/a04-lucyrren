6218a5ae3e6bd5ad53861ab06d0fd4c2d68628ce
Lucy <lucyren@email.unc.edu>
2022-11-28 13:48:20 -0500

    /app/roll endpoints

--- server.js
+++ server.js
@@ -5,3 +5,20 @@ import express from "express";
 let args = minimist(process.argv.slice(2));
 
 const port = args.port || 5000;
+
+let app = express();
+app.use(express.urlencoded({extended: true}));
+
+app.get('/app', (req, res) => {
+    res.type('html');
+    res.status(200).send('200 OK');
+});
+
+app.get('/app/roll', (req, res) => {
+    res.send(JSON.stringify(roll(6, 2, 1)));
+});
+
+app.get('/app/roll/', (req, res) => {
+    res.send(JSON.stringify(roll(parseInt(req.body.sides), parseInt(req.body.dice), parseInt(req.body.rolls))));
+});
+
