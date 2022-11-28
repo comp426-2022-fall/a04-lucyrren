524447a8621b4000193c106420609e3d8a8371c6
Lucy <lucyren@email.unc.edu>
2022-11-28 14:38:19 -0500

    finished?

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import { roll } from "../lib/roll.js";
+import { roll } from "./lib/roll.js";
 import minimist from "minimist";
 import express from "express";
 
@@ -8,9 +8,9 @@ const port = args.port || 5000;
 
 let app = express();
 app.use(express.urlencoded({extended: true}));
+app.listen(port);
 
 app.get('/app', (req, res) => {
-    res.type('html');
     res.status(200).send('200 OK');
 });
 
@@ -20,4 +20,20 @@ app.get('/app/roll', (req, res) => {
 
 app.post('/app/roll/', (req, res) => {
     res.status(200).send(JSON.stringify(roll(parseInt(req.body.sides), parseInt(req.body.dice), parseInt(req.body.rolls))));
+});
+
+app.get('/app/roll/:sides', (req, res) => {
+    res.status(200).send(JSON.stringify(roll(parseInt(req.params.sides), 2, 1)));
+});
+
+app.get('/app/roll/:sides/:dice/', (req, res) => {
+    res.status(200).send(JSON.stringify(roll(parseInt(req.params.sides), parseInt(req.params.dice), 1)));
+});
+
+app.get('/app/roll/:sides/:dice/:rolls', (req, res) => {
+    res.status(200).send(JSON.stringify(roll(parseInt(req.params.sides), parseInt(req.params.dice), parseInt(req.params.rolls))));
+});
+
+app.get('*', (req, res) => {
+    res.status(404).send('404 NOT FOUND');
 });
