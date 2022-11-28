a0120c30696e3f0112ac3ac223be8ebc48f7814c
Lucy <lucyren@email.unc.edu>
2022-11-28 13:49:33 -0500

    fix /app/roll

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ app.get('/app/roll', (req, res) => {
     res.send(JSON.stringify(roll(6, 2, 1)));
 });
 
-app.get('/app/roll/', (req, res) => {
+app.post('/app/roll/', (req, res) => {
     res.send(JSON.stringify(roll(parseInt(req.body.sides), parseInt(req.body.dice), parseInt(req.body.rolls))));
 });
-
